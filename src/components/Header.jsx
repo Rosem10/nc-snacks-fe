0ad0001bb82ba5b2569852f730d0626e09d675e1
Rose Mullan <rfmullan@gmail.com>
2023-06-01 16:32:30 +0100

    prepare repo for hosting

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,13 @@ import { Link } from 'react-router-dom'
 
 function Header() { 
     return (
-        <>
+        <div className="Header">
             <h1>NC Snacks</h1>
             <nav>
-                <Link to="/snacks">Snacks </Link>
-                <Link to="/">Home</Link>
+                <Link to="/snacks" className='link'>Snacks </Link>
+                <Link to="/" className='link'>Home</Link>
             </nav>
-        </>
+        </div>
     );
 }
 
