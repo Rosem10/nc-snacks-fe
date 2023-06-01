7f06b84f3c7f1927a6492547771cb9b8233d285a
Rose Mullan <rfmullan@gmail.com>
2023-06-01 20:09:30 +0100

    fix header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
         <div className="Header">
             <h1>NC Snacks</h1>
             <nav>
-                <Link to="/snacks" className='link'>Snackos </Link>
+                <Link to="/snacks" className='link'>Snacks </Link>
                 <Link to="/" className='link'>Home</Link>
             </nav>
         </div>
